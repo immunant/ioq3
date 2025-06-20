71aaf7d6e466bf05a009ff53fdedde9296d23208
Andrei Homescu <ah@immunant.com>
2025-06-19 17:27:15 -0700

    Fix build by avoiding null_mut with extern types

--- quake3-rs/ioquake3/src/sdl/sdl_input.rs
+++ quake3-rs/ioquake3/src/sdl/sdl_input.rs
@@ -1115,9 +1115,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 static mut in_keyboardDebug: *mut cvar_t = std::ptr::null_mut();
 
-static mut gamepad: *mut SDL_GameController = std::ptr::null_mut();
+static mut gamepad: *mut SDL_GameController = 0 as *mut SDL_GameController;
 
-static mut stick: *mut SDL_Joystick = std::ptr::null_mut();
+static mut stick: *mut SDL_Joystick = 0 as *mut SDL_Joystick;
 
 static mut mouseAvailable: qboolean = qfalse;
 
@@ -1139,7 +1139,7 @@ static mut vidRestartTime: i32 = 0 as i32;
 
 static mut in_eventTime: i32 = 0 as i32;
 
-static mut SDL_window: *mut SDL_Window = std::ptr::null_mut();
+static mut SDL_window: *mut SDL_Window = 0 as *mut SDL_Window;
 /*
 ===============
 IN_PrintKey
